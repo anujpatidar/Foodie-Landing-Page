50999f9f1dbd47196703cf9c68b75c5602e4d58f
Anuj Patidar <82809623+psdartist88@users.noreply.github.com>
2023-03-19 23:34:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+
+
+document.querySelector("#el1").addEventListener("click",function(){
+    document.querySelector("#dishes").style.transform = "translate(-50%, -68%) rotate(83deg)";
+    document.querySelector("#el2").style.backgroundColor = "transparent";
+    document.querySelector("#el3").style.backgroundColor = "transparent";
+    document.querySelector("#el4").style.backgroundColor = "transparent";
+    document.querySelector("#el1").style.backgroundColor = "rgba(255, 255, 255, 0.6)";
+    document.querySelector("#content h3").innerHTML = "#1 Most Loved dish";
+    document.querySelector("#so").innerHTML = "SOYA";
+    document.querySelector("#rating>h2").textContent = "5.0";
+    document.querySelector("#box>h2").textContent = "Chef Harpal";
+    document.querySelector("#box>h3").textContent = "Hyderabad";
+    document.querySelector("#so2").innerHTML = "BIRYANI";
+    
+
+})
+document.querySelector("#el2").addEventListener("click",function(){
+    document.querySelector("#dishes").style.transform = "translate(-50%, -68%) rotate(172deg)";
+    document.querySelector("#el1").style.backgroundColor = "transparent";
+    document.querySelector("#el3").style.backgroundColor = "transparent";
+    document.querySelector("#el4").style.backgroundColor = "transparent";
+    document.querySelector("#el2").style.backgroundColor = "rgba(255, 255, 255, 0.6)";
+    document.querySelector("#content h3").innerHTML = "#2 Most Loved dish";
+    document.querySelector("#so").innerHTML = "JALEBI";
+    document.querySelector("#so2").innerHTML = "RABADI";
+    document.querySelector("#rating>h2").textContent = "4.9";
+    document.querySelector("#box>h2").textContent = "Chef Rajshree";
+    document.querySelector("#box>h3").textContent = "Bhopal";
+    
+
+})
+document.querySelector("#el3").addEventListener("click",function(){
+    document.querySelector("#dishes").style.transform = "translate(-50%, -68%) rotate(263deg)";
+    document.querySelector("#el1").style.backgroundColor = "transparent";
+    document.querySelector("#el2").style.backgroundColor = "transparent";
+    document.querySelector("#el4").style.backgroundColor = "transparent";
+    document.querySelector("#el3").style.backgroundColor = "rgba(255, 255, 255, 0.6)";
+    document.querySelector("#content h3").innerHTML = "#3 Most Loved dish";
+    document.querySelector("#so").innerHTML = "MOTICHOOR";
+    document.querySelector("#so2").innerHTML = "LADDOO";
+    document.querySelector("#rating>h2").textContent = "4.8";
+    document.querySelector("#box>h2").textContent = "Chef Ruchi";
+    document.querySelector("#box>h3").textContent = "Indore";
+
+})
+document.querySelector("#el4").addEventListener("click",function(){
+    document.querySelector("#dishes").style.transform = "translate(-50%, -68%) rotate(353deg)";
+    document.querySelector("#el1").style.backgroundColor = "transparent";
+    document.querySelector("#el2").style.backgroundColor = "transparent";
+    document.querySelector("#el3").style.backgroundColor = "transparent";
+    document.querySelector("#el4").style.backgroundColor = "rgba(255, 255, 255, 0.6)";
+    document.querySelector("#content h3").innerHTML = "#4 Most Loved dish";
+    document.querySelector("#so").innerHTML = "KHAMAN";
+    document.querySelector("#so2").innerHTML = "DHOKLA";
+    document.querySelector("#rating>h2").textContent = "4.7";
+    document.querySelector("#box>h2").textContent = "Chef Sanjeev";
+    document.querySelector("#box>h3").textContent = "Vadodra";
+
+})
+
